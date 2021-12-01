01c7cfa6307086adbe98b6b46f07a6e1b1f1410f
NiKiKRaD <nikikrad228@gmail.com>
2021-12-01 23:05:34 +0300

    Sponsors is remake

--- routes/Sponsors.js
+++ routes/Sponsors.js
@@ -30,9 +30,9 @@ router.get("/", (req,res) => {
       const sponsors = rows.map((row) => {//rows - ответ гет запроса
         return {
           id: row.idSponsors,
-          name: row.name,
-          number: row.number,
-          mail: row.mail
+          sponsorName: row.sponsorName,
+          sponsorNumber: row.sponsorNumber,
+          sponsorMail: row.sponsorMail
         }
       })
   
@@ -44,8 +44,8 @@ router.get("/", (req,res) => {
   router.post("/create", (req, res) => {//is ready
     const connection = getConnection()
   
-    const queryString = "INSERT INTO `Sponsors` (name, number, mail, idSportClubs) VALUES (?, ?, ?, ?)"
-    getConnection().query(queryString, [req.body.name, req.body.number, req.body.mail, req.body.idSportClubs], (err, results, fields) => {
+    const queryString = "INSERT INTO `Sponsors` (sponsorName, sponsorNumber, sponsorMail, idSportClubs) VALUES (?, ?, ?, ?)"
+    getConnection().query(queryString, [req.body.sponsorName, req.body.sponsorNumber, req.body.sponsorMail, req.body.idSportClubs], (err, results, fields) => {
       if (err) {
         console.log(err)
         res.sendStatus(500)
@@ -76,8 +76,8 @@ router.get("/", (req,res) => {
   router.put("/update/:id", (req, res) => {//is ready
     const connection = getConnection()
   
-    const queryString = "UPDATE `Sponsors` SET name = ?, number = ?, mail = ?, idSportClubs = ? WHERE idSponsors = ?"
-    getConnection().query(queryString, [req.body.name, req.body.number, req.body.mail, req.body.idSportClubs, req.params.id], (err, results, fields) => {
+    const queryString = "UPDATE `Sponsors` SET sponsorName = ?, sponsorNumber = ?, sponsorMail = ?, idSportClubs = ? WHERE idSponsors = ?"
+    getConnection().query(queryString, [req.body.sponsorName, req.body.sponsorNumber, req.body.sponsorMail, req.body.idSportClubs, req.params.id], (err, results, fields) => {
       if (err) {
         console.log(err)
         res.sendStatus(500)
